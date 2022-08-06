25c8cb86b39a10fd9466cbb48838c2d425715442
ushmabhatt <ushma.bhatt@somaiya.edu>
2022-08-06 01:49:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,9 @@ try:
    else:
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
       streamlit.text(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+ 
+
 except URLError as e:
     streamlit.error()
 
